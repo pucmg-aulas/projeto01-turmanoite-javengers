11736f4e041ac93bfea892f1162dafbba6125e90
Gustavo Delfino Guimarães <145400944+gdelfino01@users.noreply.github.com>
2024-04-23 15:19:19 -0300

    Update Reserva.java

--- codigo/restaurante/src/Reserva.java
+++ codigo/restaurante/src/Reserva.java
@@ -1,21 +1,71 @@
-import java.time.*;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
 
+package com.mycompany.reserva;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Gustavo Delfino
+ */
 public class Reserva {
+
     private Cliente cliente;
     private boolean estaNaEspera;
-    private int quantidadePessoas;
-    private LocalDateTime dataHoraEntrada;
-    private LocalDateTime dataHoraSaida;
-
-    public Reserva(Cliente cliente, boolean estaNaEspera, int quantidadePessoas, LocalDateTime dataHoraEntrada, LocalDateTime dataHoraSaida){
+    private int quantPessoas;
+    Date dataHoraEntrada;
+    Date dataHoraSaida;
+    
+    public Reserva(Cliente cliente, boolean estaNaEspera, int quantPessoas, Date dataHoraEntrada){
         this.cliente = cliente;
         this.estaNaEspera = estaNaEspera;
-        this.quantidadePessoas = quantidadePessoas;
+        this.quantPessoas = quantPessoas;
         this.dataHoraEntrada = dataHoraEntrada;
+        
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public boolean isEstaNaEspera() {
+        return estaNaEspera;
+    }
+
+    public void setEstaNaEspera(boolean estaNaEspera) {
+        this.estaNaEspera = estaNaEspera;
+    }
+
+    public int getQuantPessoas() {
+        return quantPessoas;
+    }
+
+    public void setQuantPessoas(int quantPessoas) {
+        this.quantPessoas = quantPessoas;
+    }
+
+    public Date getDataHoraEntrada() {
+        return dataHoraEntrada;
+    }
+
+    public void setDataHoraEntrada(Date dataHoraEntrada) {
+        this.dataHoraEntrada = dataHoraEntrada;
+    }
+
+    public Date getDataHoraSaida() {
+        return dataHoraSaida;
+    }
+
+    public void setDataHoraSaida(Date dataHoraSaida) {
         this.dataHoraSaida = dataHoraSaida;
     }
-
-    public void verificaQuantPessoas(int quantidadePessoas){
-
-    }
+    
+    
 }
