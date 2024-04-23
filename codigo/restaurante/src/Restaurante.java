2766c0be9c1977dc8b13960b63929982b8514bd2
João Pedro Santana Marques <32452904+jpsantanam@users.noreply.github.com>
2024-04-23 17:31:06 -0300

    Atualiza classe Restaurante

--- codigo/restaurante/src/Restaurante.java
+++ codigo/restaurante/src/Restaurante.java
@@ -60,6 +60,14 @@ public class Restaurante {
         verificaListaDeEspera(); //sempre que uma mesa ficar livre, faz a verificação da lista de espera
     }
 
+    public void adicionaListaDeEspera(Reserva reserva) {
+        listaDeEspera.add(reserva); //adiciona uma reserva na lista de espera
+    }
+
+    public void removeListaDeEspera(Reserva reserva) {
+        listaDeEspera.remove(reserva); //remove uma reserva da lista de espera
+    }
+
     public boolean fazReservaDeMesa(Reserva reserva) {
         for (Mesa mesa : mesasDisponiveis) { //itera as mesas disponíveis
             if (mesa.getQuantCadeiras() >= reserva.getQuantPessoas()) { //se houver mesa disponível que atende a quantidade de pessoas:
@@ -81,3 +89,4 @@ public class Restaurante {
         return false; //retorna false se não tiver mesas disponíveis
     }
 }
+
