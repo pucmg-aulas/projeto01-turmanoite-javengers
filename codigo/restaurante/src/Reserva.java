a83122952550379416383d1f77ef6e168d881530
Júlia Medeiros <129974426+JuliaMedeir0s@users.noreply.github.com>
2024-04-23 14:15:17 -0300

    Update Reserva.java

--- codigo/restaurante/src/Reserva.java
+++ codigo/restaurante/src/Reserva.java
@@ -3,19 +3,19 @@ import java.time.*;
 public class Reserva {
     private Cliente cliente;
     private boolean estaNaEspera;
-    private int quantPessoas;
+    private int quantidadePessoas;
     private LocalDateTime dataHoraEntrada;
     private LocalDateTime dataHoraSaida;
 
-    public Reserva(Cliente cliente, boolean estaNaEspera, int quantPessoas, LocalDateTime dataHoraEntrada, LocalDateTime dataHoraSaida){
+    public Reserva(Cliente cliente, boolean estaNaEspera, int quantidadePessoas, LocalDateTime dataHoraEntrada, LocalDateTime dataHoraSaida){
         this.cliente = cliente;
         this.estaNaEspera = estaNaEspera;
-        this.quantPessoas = quantPessoas;
+        this.quantidadePessoas = quantidadePessoas;
         this.dataHoraEntrada = dataHoraEntrada;
         this.dataHoraSaida = dataHoraSaida;
     }
 
-    public void verificaQuantPessoas(int quantPessoas){
+    public void verificaQuantPessoas(int quantidadePessoas){
 
     }
 }
