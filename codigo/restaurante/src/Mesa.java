f0432906a1fe6bbe935c7cb9815047664b509d78
Júlia Medeiros <129974426+JuliaMedeir0s@users.noreply.github.com>
2024-04-23 14:20:06 -0300

    Update Mesa.java

--- codigo/restaurante/src/Mesa.java
+++ codigo/restaurante/src/Mesa.java
@@ -1,21 +1,11 @@
 public class Mesa {
-    private int quantCadeiras;
-    private boolean ocupada;
+    private int quantidadeCadeiras;
 
-    public Mesa(int quantCadeiras) {
-        this.quantCadeiras = quantCadeiras;
-        this.ocupada = false;
+    public Mesa(int quantidadeCadeiras) {
+        this.quantidadeCadeiras = quantidadeCadeiras;
     }
 
-    public int getQuantCadeiras() { // para outra classe conseguir identificar a capacidade da mesa
-        return quantCadeiras;
-    }
-
-    public boolean isOcupada() { // retorna o status da mesa
-        return ocupada;
-    }
-
-    public void setOcupada(boolean ocupada) { // mudar o status da mesa quando um cliente ocupa/libera
-        this.ocupada = ocupada;
+    public int getQuantCadeiras() { 
+        return quantidadeCadeiras;
     }
 }
