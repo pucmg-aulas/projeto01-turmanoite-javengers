4ef3c03486a11e406e8ebf83d4efd982e24c6ef6
Gustavo Delfino Guimarães <145400944+gdelfino01@users.noreply.github.com>
2024-04-29 14:48:58 -0300

    Update Reserva.java

--- codigo/restaurante/src/Reserva.java
+++ codigo/restaurante/src/Reserva.java
@@ -4,44 +4,15 @@ public class Reserva {
 
     private Cliente cliente;
     private int quantPessoas;
+    private Mesa mesa;
     LocalDateTime dataHoraEntrada;
     LocalDateTime dataHoraSaida;
     
-    public Reserva(Cliente cliente, int quantPessoas, LocalDateTime dataHoraEntrada){
+    public Reserva(Cliente cliente, int quantPessoas, LocalDateTime dataHoraEntrada, Mesa mesa){
         this.cliente = cliente;
+        this.mesa
         this.quantPessoas = quantPessoas;
         this.dataHoraEntrada = dataHoraEntrada;
     }
 
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public int getQuantPessoas() {
-        return quantPessoas;
-    }
-
-    public void setQuantPessoas(int quantPessoas) {
-        this.quantPessoas = quantPessoas;
-    }
-
-    public LocalDateTime getDataHoraEntrada() {
-        return dataHoraEntrada;
-    }
-
-    public void setDataHoraEntrada(LocalDateTime dataHoraEntrada) {
-        this.dataHoraEntrada = dataHoraEntrada;
-    }
-
-    public LocalDateTime getDataHoraSaida() {
-        return dataHoraSaida;
-    }
-
-    public void setDataHoraSaida(LocalDateTime dataHoraSaida) {
-        this.dataHoraSaida = dataHoraSaida;
-    }
 }
