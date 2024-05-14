e49a21bdba17a384278314bd055e111545727674
Gustavo Delfino Guimarães <delfinoguimaraes1@gmail.com>
2024-05-14 13:25:34 -0300

    implementando classe alimento

---
+++ codigo/restaurante/src/Alimento.java
@@ -0,0 +1,35 @@
+public class Alimento {
+    private String nome;
+    private String descricao;
+    private double valor;
+
+    public Alimento(String nome, String descricao, double valor) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
