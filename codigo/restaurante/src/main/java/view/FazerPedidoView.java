5a52cf3c95d91c34a839436ff854dc18ac718171
JuliaMedeir0s <julia.medeiros1159@gmail.com>
2024-06-04 15:26:27 -0300

    Pedido view

---
+++ codigo/restaurante/src/main/java/view/FazerPedidoView.java
@@ -0,0 +1,53 @@
+package main.java.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FazerPedidoView extends JFrame {
+
+    private JComboBox itemComboBox;
+    private JTextField quantidadeTextField;
+    private JButton salvarPedidoButton;;
+
+    public FazerPedidoView() {
+
+        setTitle("Fazer Pedido");
+        setSize(400, 600);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanel selecaoItemPanel = new JPanel();
+        selecaoItemPanel.setLayout(new FlowLayout());
+
+        itemComboBox = new JComboBox();
+        selecaoItemPanel.add(itemComboBox);
+
+        JPanel quantidadeEBotaoPanel = new JPanel();
+        quantidadeEBotaoPanel.setLayout(new FlowLayout());
+
+        quantidadeTextField = new JTextField(10);
+        quantidadeEBotaoPanel.add(quantidadeTextField);
+
+        salvarPedidoButton = new JButton("Salvar Pedido");
+        quantidadeEBotaoPanel.add(salvarPedidoButton);
+
+        add(selecaoItemPanel, BorderLayout.NORTH);
+        add(quantidadeEBotaoPanel, BorderLayout.CENTER);
+    }
+
+    public JComboBox getItemComboBox() {
+        return this.itemComboBox;
+    }
+
+    public JTextField getQuantidadeTextField() {
+        return this.quantidadeTextField;
+    }
+
+    public JButton getSalvarPedidoButton() {
+        return this.salvarPedidoButton;
+    }
+
+    public void setQuantidade(int quantidade) {
+        quantidadeTextField.setText(String.valueOf(quantidade));
+    }
+}
